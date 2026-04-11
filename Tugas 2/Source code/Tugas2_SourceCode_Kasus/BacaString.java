86a71641c6112acd084543777efa91e23f935eea
leonpratama <leonpratama19@gmail.com>
2026-04-11 13:13:14 +0800

    BacaString.java

--- Tugas 2/Source code/Tugas2_SourceCode_Kasus/BacaString.java
+++ Tugas 2/Source code/Tugas2_SourceCode_Kasus/BacaString.java
@@ -1,3 +1,8 @@
+/* NIM   : 13020240269
+Nama  : Leon Octa Pratama
+Hari/Tanggal : 11 April 2026
+Waktu : 13:00*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
